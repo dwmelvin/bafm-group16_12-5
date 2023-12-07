947577aa210f02122d0547f2d70788640bdeead4
Jack <jawilson18@crimson.ua.edu>
2023-12-06 20:04:52 -0600

    Empty

--- api/Models/Admin.cs
+++
@@ -1,8 +0,0 @@
-namespace api.Models
-{
-    public class Admin : User
-    {
-        public string AdminName { get; set; }
-
-    }
-}
--- api/Models/BookedSlot.cs
+++
@@ -1,9 +0,0 @@
-namespace api.Models
-{
-    public class BookedSlot
-    {
-        public int BookedSlotID { get; set; }
-        public int EventSlotID { get; set; }
-        public int BusinessID { get; set; }
-    }
-}
