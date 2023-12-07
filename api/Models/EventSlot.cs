0c52f600c5ff43b9fffe97c047d73f53e784876c
Jack <jawilson18@crimson.ua.edu>
2023-12-07 10:53:37 -0600

    EventModels

---
+++ api/Models/EventSlot.cs
@@ -0,0 +1,9 @@
+namespace api.Controllers
+{
+    public class EventSlot
+    {
+        public int EventSlotID {get; set;}
+        public string Date {get; set;}
+        public string Lot {get; set;}
+    }
+}
